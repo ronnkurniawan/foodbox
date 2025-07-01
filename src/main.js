434a6349675989edf1a6c8ca65e23e4a21b84546
ronnkurniawan <ronnkurniawan@gmail.com>
2025-07-01 11:31:45 +0700

    home desktop done

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
 import './style.css'
 
+function backToTop() {
+  document.querySelector('.back-top_btn').addEventListener('click', () =>{
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  })
+}
+
+backToTop();
